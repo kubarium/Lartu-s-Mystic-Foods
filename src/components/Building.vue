8af58931bbcb57f8759392ef4105d9f8f5a256b2
kubarium <iam@denizkumsal.com>
2018-09-19 09:44:45 -0400

    grids it

--- src/components/Building.vue
+++ src/components/Building.vue
@@ -23,25 +23,31 @@ export default {
   outline: 1px solid yellow;
   /* position: absolute; */
   display: grid;
-  grid-template-areas:
+  /* grid-template-areas:
     "left-wall counter right-wall"
     "left-wall floor right-wall"
-    "left-wall bottom-wall right-wall";
-  grid-template-rows: 1fr 140px 1fr;
-  grid-template-columns: 1fr 32px 1fr;
+    "left-wall bottom-wall right-wall"; */
+  grid-template-rows: 140px auto 32px;
+  grid-template-columns: [wall] 32px [floor] 1fr [wall] 32px;
   background: url("../assets/floor.jpg");
 }
 .counter {
   grid-area: "counter";
-  width: calc(100% - 32px - 32px);
-  height: 40px;
+  grid-column: 2 / 3;
+  grid-row: 1 / 2;
+  height: 140px;
   background: red;
 }
 .floor {
   grid-area: "floor";
+  grid-column: 2 / 3;
+  grid-row: 2 / 3;
+  background: blue;
 }
 .left-wall {
   grid-area: "left-wall";
+  grid-column: 1 / 2;
+  grid-row: 1 / 3;
   outline: 1px solid lightgreen;
   background: url("../assets/left-wall.jpg");
   width: 32px;
@@ -49,6 +55,8 @@ export default {
 }
 .bottom-wall {
   grid-area: "bottom-wall";
+  grid-column: 1 / 4;
+  grid-row: 3 / 4;
   outline: 1px solid burlywood;
   background: url("../assets/bottom-wall.jpg");
   height: 32px;
@@ -56,6 +64,8 @@ export default {
 }
 .right-wall {
   grid-area: "right-wall";
+  grid-column: 3 / 4;
+  grid-row: 1 / 3;
   outline: 1px solid red;
   background: url("../assets/right-wall.jpg");
   width: 32px;
